6e9f218da5dbacff759c7d9bcf4efc6b423d9f12
y-homma <yoshi_h_h46@hotmail.co.jp>
2022-10-24 15:56:12 +0900

    test function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rand::random;
+
 fn main() {
-    println!("Hello, world!");
+    let random_number: i32 = random();
+    println!("{}", random_number);
+}
+
+fn div(x: i32, y: i32) -> i32 {
+    x / y
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn div_test() {
+        assert_eq!(div(10, 3), 3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn div_panic_test() {
+        div(2, 0);
+    }
 }
